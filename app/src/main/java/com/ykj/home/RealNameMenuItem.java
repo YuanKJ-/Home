a40f6d3b354e752896d508d79dde5e8d74d20af5
Kejie Yuan <kejie@texchi.com>
2019-07-21 23:44:42 +0800

    commit

--- app/src/main/java/com/ykj/home/RealNameMenuItem.java
+++ app/src/main/java/com/ykj/home/RealNameMenuItem.java
@@ -3,8 +3,12 @@ package com.ykj.home;
 import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
+import android.view.View;
 
 public class RealNameMenuItem extends ConstraintLayout {
+
+    private View realNameSuccess;
+    private View realNameFail;
 
     public RealNameMenuItem(Context context) {
         this(context, null);
@@ -17,7 +21,19 @@ public class RealNameMenuItem extends ConstraintLayout {
     public RealNameMenuItem(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         inflate(context, R.layout.real_name_slide_menu, this);
+        realNameSuccess = findViewById(R.id.real_name_success);
+        realNameFail = findViewById(R.id.real_name_fail);
+        setRealName(true);
     }
 
+    public void setRealName(boolean success) {
+        if (success) {
+            realNameSuccess.setVisibility(VISIBLE);
+            realNameFail.setVisibility(GONE);
+        } else {
+            realNameSuccess.setVisibility(GONE);
+            realNameFail.setVisibility(VISIBLE);
+        }
+    }
 
 }
